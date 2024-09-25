416252dbfe1a8fcff7d775dd9598f43e7a70c02f
anus <anusyounus124@gmail.com>
2024-09-25 17:54:40 +0500

    modified class 15db

--- Class 15 (DB)/src/app/home/page.tsx
+++ Class 15 (DB)/src/app/home/page.tsx
@@ -2,36 +2,31 @@
 
 import auth, { handleLogout } from "@/firebase/firebaseAuth"
 import db, { createTodo, fetchTodos } from "@/firebase/firebaseDb"
-import { collection, DocumentData, onSnapshot, query, where } from "firebase/firestore"
+import { onAuthStateChanged } from "firebase/auth"
+import { collection, DocumentData, onSnapshot, query, Unsubscribe, where } from "firebase/firestore"
+import Link from "next/link"
 import { useEffect, useState } from "react"
 
 export default function Main() {
     const [todo, setTodo] = useState("")
     const [allTodos, setAllTodos] = useState<DocumentData[]>([])
     useEffect(() => {
-        let collectionRef = collection(db, "todos");
-        let currentUserUID = localStorage.getItem('uid')
-        let condition = where("uid", "==", currentUserUID);
-        let q = query(collectionRef, condition);
-        let allTodosClone = [...allTodos]
-        onSnapshot(q, (querySnapshot) => {
-            querySnapshot.docChanges().forEach((change) => {
-                if (change.type === "added") {
-                    let todo = change.doc.data();
-                    todo.id = change.doc.id;
-                    allTodosClone.push(todo);
-                    setAllTodos([...allTodosClone])
-                }
-                if (change.type === "modified") {
-                }
-                if (change.type === "removed") {
-                }
-            })
+        const stateChangedDispatch = onAuthStateChanged(auth, (user) => {
+            if(user) {
+                handleFetch()
+            }
         })
-        // handleFetch()
+        return () => {
+            if(realTimeDispatch) {
+                realTimeDispatch()
+                stateChangedDispatch()
+                console.log("Component Unmount")
+            }
+        }
         // console.log(allTodos);
-        
     }, [])
+
+    let realTimeDispatch: Unsubscribe
 
     // const handleFetch = async () => {
     //     const allTodos = await fetchTodos()
@@ -43,11 +38,32 @@ export default function Main() {
     // }
 
     const handleFetch = () => {
-        
+        let collectionRef = collection(db, "todos");
+        let currentUserUID = auth.currentUser?.uid
+        let condition = where("uid", "==", currentUserUID);
+        let q = query(collectionRef, condition);
+        let allTodosClone = [...allTodos]
+        realTimeDispatch = onSnapshot(q, (querySnapshot) => {
+            querySnapshot.docChanges().forEach((change) => {
+                if (change.type === "added") {
+                    let todo = change.doc.data();
+                    todo.id = change.doc.id;
+                    allTodosClone.push(todo);
+                    setAllTodos([...allTodosClone])
+                }
+                if (change.type === "modified") {
+                    console.log("data modified")
+                }
+                if (change.type === "removed") {
+                    console.log("data removed")
+                }
+            })
+        })
     }
     return (
         <>
             <div className="">
+                <Link href={'/about'}>About</Link>
                 <div className="">
                     <p>Welcome {auth.currentUser?.email}</p>
                     <button onClick={handleLogout}>logout</button>
@@ -61,8 +77,8 @@ export default function Main() {
             </div>
             {
                 allTodos.length > 0 ?
-                    allTodos.map(({ todo, id }) => <h1 key={id}>{todo}</h1>) :
-                    <>Hello</>
+                    allTodos.map(({ todo, id }) => <div style={{justifyContent: "space-between", display: "flex", width: "90%", border: "1px solid red", margin: "0 5em"}} key={id}><p>{todo}</p> <div style={{display: "fl"}}><button>Edit</button><button>Delete</button></div></div>) :
+                    <>Not todos avaliable</>
             }
         </>
     )
